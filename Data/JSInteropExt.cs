60baae0ec3de150eb8ed63c34f140edcbcfa571e
Juan Restrepo C <juan_restrepo23202@elpoli.edu.co>
2025-05-26 16:14:04 -0500

    Se agrega el servicio de reportes, unicamente con excel.

---
+++ Data/JSInteropExt.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace ProtelAppT.Data
+{
+    public static class JSInteropExt
+    {
+        public static async Task SaveAsFileAsync(this IJSRuntime js, string fileName, byte[] data, string type="application/octet-stream")
+        {
+            await js.InvokeAsync<object>("JSInteropExt.saveAsFile", fileName, type, Convert.ToBase64String(data));
+        }
+    }
+}
